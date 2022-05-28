1f04a4b6c199b9bbebe00632f4b2270b731e7fcd
RayBar72 <3963@holbertonschool.com>
2022-05-28 06:34:09 -0300

    Checked semistandard

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,13 @@
-$(function (){
+$(function () {
   const lAmen = {};
+
   $('div.amenities li input')
-  .change( function () {
-    if ($(this).is(':checked')) {
-      lAmen[($(this).attr('data-id'))];
-    } else {
-      delete lAmen[$(this).attr('data-id')];
-    }
-    $('div.amenities h4').html(Object.values(lAmen).join(', ') || '&nbsp;');
-  });
-})
+    .change(function () {
+      if ($(this).is(':checked')) {
+        lAmen[($(this).attr('data-id'))] = $(this).attr('data-name');
+      } else {
+        delete lAmen[$(this).attr('data-id')];
+      }
+      $('div.amenities h4').html(Object.values(lAmen).join(', ') || '&nbsp;');
+    });
+});
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,18 +1,21 @@
-$(function (){
+$(function () {
   const lAmen = {};
+
   $('div.amenities li input')
-  .change( function () {
-    if ($(this).is(':checked')) {
-      lAmen[($(this).attr('data-id'))];
-    } else {
-      delete lAmen[$(this).attr('data-id')];
-    }
-    $('div.amenities h4').html(Object.values(lAmen).join(', ') || '&nbsp;');
-  });
+    .change(function () {
+      if ($(this).is(':checked')) {
+        lAmen[($(this).attr('data-id'))] = $(this).attr('data-name');
+      } else {
+        delete lAmen[$(this).attr('data-id')];
+      }
+      $('div.amenities h4').html(Object.values(lAmen).join(', ') || '&nbsp;');
+    });
 
   $.getJSON('http://0.0.0.0:5001/api/v1/status', (data) => {
-    if (data.status == 'OK') {
-      $('DIV#api_status').removeClass('availabe');
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
     }
   });
-})
+});
